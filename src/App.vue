1d392283f980c87204d161810308a4b097db724a
fernandorjesus <fernando.jesus@solfacil.com.br>
2021-01-18 22:26:53 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Demo />
+  </div>
+</template>
+
+<script>
+import Demo from './Demo.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Demo,
+  },
+  data() {
+    return {
+      listOptions: [
+        {
+          label: 'Subscribe',
+          value: 'subscribe',
+        },
+        {
+          label: 'Unsubscribe',
+          value: 'unsubscribe',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
